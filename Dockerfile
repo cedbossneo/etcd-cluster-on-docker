1651756b47e64ea07b0d4923f9d5ba771d6e7905
Cedric Hauber <cedric.hauber@wescale.fr>
2016-09-12 17:39:01 +0200

    Some tweeks

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM        alpine:latest
-RUN         apk add --update bash ca-certificates openssl tar drill net-tools netcat-openbsd && \
+RUN         apk add --update bash ca-certificates openssl tar drill net-tools curl netcat-openbsd && \
             wget https://github.com/coreos/etcd/releases/download/v3.0.7/etcd-v3.0.7-linux-amd64.tar.gz && \
             tar xzvf etcd-v3.0.7-linux-amd64.tar.gz && \
             mv etcd-v3.0.7-linux-amd64/etcd* /bin/ && \
@@ -7,5 +7,6 @@ RUN         apk add --update bash ca-certificates openssl tar drill net-tools ne
             rm -Rf etcd-v3.0.7-linux-amd64* /var/cache/apk/*
 EXPOSE      2379 2380
 ADD         /bin/etcd_init.sh /bin/etcd_init.sh
-RUN         chmod +x /bin/etcd_init.sh
-CMD         ["/bin/etcd_init.sh"]            
+ADD         /bin/etcd_proxy.sh /bin/etcd_proxy.sh
+RUN         chmod +x /bin/etcd_init.sh /bin/etcd_proxy.sh
+CMD         ["/bin/etcd_init.sh"]
--- bin/etcd_init.sh
+++ bin/etcd_init.sh
@@ -5,13 +5,10 @@
 # lockfile.  Failing that, the next step is that it will try
 # existing directories in the range of 0..NODE_COUNT, exclusive.
 [ -z "$FS_PATH" ] && echo "Need to set FS_PATH" && exit 1;
-[ -z "$NODE_COUNT" ] && echo "Need to set NODE_COUNT" && exit 1;
 
 APP_NAME="etcd"
 LOCKFILE="$APP_NAME.lock"
-CONTAINER_IP_FILE="$APP_NAME.node_ip"
-CONTAINER_ID_FILE="$APP_NAME.node_id"
-AUTHORITATIVE_ID=0
+AUTHORITATIVE_ID="0"
 
 ip a
 
@@ -33,22 +30,8 @@ launch_etcd() {
     fi
 
     (
-        /bin/etcd -data-dir="$DATA_DIR" -initial-cluster-state $CLUSTER_STATE -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 -advertise-client-urls http://$IP:2379  -listen-peer-urls http://$IP:2380,http://$IP:7001 -initial-advertise-peer-urls http://$MY_IP:2380 -name "$APP_NAME-$ID" -initial-cluster "$INITIAL_CLUSTER"
+        /bin/etcd --initial-cluster-token etcd-cluster -data-dir="$DATA_DIR" -initial-cluster-state $CLUSTER_STATE -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 -advertise-client-urls http://$IP:2379  -listen-peer-urls http://$IP:2380,http://$IP:7001 -initial-advertise-peer-urls http://$IP:2380 -name "$APP_NAME-$ID" -initial-cluster "$INITIAL_CLUSTER"
     ) &
-
-    local PID=""
-    for f in {1..5}; do
-    echo ps auxwww | grep $MY_IP | grep etcd | grep -v grep | awk '{print $1}'
-        local PROCESS_PID=`ps auxwww | grep $MY_IP | grep etcd | grep -v grep | awk '{print $1}'`
-        if [ $? -eq 0 ]; then
-            PID="$PROCESS_PID"
-            break
-        else
-            sleep 1
-        fi
-    done
-
-    ETCD_PID=$PID
 }
 
 start_app() {
@@ -64,7 +47,6 @@ start_app() {
     ID=$2
     WORKINGDIR="$DATADIR/$ID"
     APP_DATA_DIR="$WORKINGDIR/$APP_NAME/data"
-    CONTAINER_ID_FILE_PATH="$WORKINGDIR/$CONTAINER_ID_FILE"
 
     if [ ! -d "$DATADIR" ]; then
         mkdir -p "$DATADIR"
@@ -72,17 +54,8 @@ start_app() {
 
     MY_IP=`ip -4 addr show scope global dev ethwe | grep inet | awk '{print $2}' | cut -d / -f 1`
     PEER_IPS=`drill $APP_NAME.weave.local | grep $APP_NAME | grep -v "\;\;" | awk '{print $5}' | grep -v $MY_IP`
-    CONTAINER_ID=""
-    if [ -e "$CONTAINER_ID_FILE_PATH" ]; then
-        CONTAINER_ID=`cat $CONTAINER_ID_FILE_PATH`
-    fi
 
     if [ -z "$PEER_IPS" ]; then
-        if [ ! -z "$CONTAINER_ID" ]; then
-            rm "$CONTAINER_ID_FILE_PATH"
-            CONTAINER_ID=""
-        fi
-
         if [ "$ID" -eq "$AUTHORITATIVE_ID" ]; then
             echo "Launching as master/initial node."
             ETCD_PID=""
@@ -97,6 +70,12 @@ start_app() {
         CLUSTER=""
         RUNNING_PEER_IPS=""
         for peer in $PEER_IPS; do
+            #  check if a member is already starting and removing them
+            STARTING=`/bin/etcdctl --endpoint=http://$peer:2379 member list | grep unstarted | awk -F "[" '{print $1}'`
+            for start in $STARTING
+            do
+              /bin/etcdctl --endpoint=http://$peer:2379 member remove $start
+            done
             # attempt to see if client port is open
             nc -w 1 $peer 2379
             if [ $? -eq 0 ]; then
@@ -131,36 +110,28 @@ start_app() {
                     echo "A known peer went down.  Bad state.  Exiting..."
                     exit 1
                 else
-                    ADD_PEER=1
-                    if [ ! -z "$CONTAINER_ID" ]; then
-                        KNOWN_ID=`/bin/etcdctl --endpoint=http://$peer:2379 member list | grep "$CONTAINER_ID" | awk '{print $1}' | sed 's/\://'`
-                        if [ ! -z "$KNOWN_ID" ]; then
-                            # update the cluster with information of the node taking over, fo shizzle
-                            echo "Updating node in the cluster.  ID: $KNOWN_ID, IP: $MY_IP"
-                            /bin/etcdctl --endpoint=http://$peer:2379 member update $KNOWN_ID http://$MY_IP:2380
-                            if [ $? -eq 1 ]; then
-                                echo "Error updating member in cluster.  ID: $KNOWN_ID, IP: $MY_IP  Exiting..."
-                                exit 1
-                            fi
-                            ADD_PEER=0
+                    KNOWN_ID=`/bin/etcdctl --endpoint=http://$peer:2379 member list | grep etcd-$ID | awk '{print $1}' | sed 's/\://'`
+                    if [ ! -z "$KNOWN_ID" ]; then
+                        # remove the existing cluster node
+                        echo "Removing node in the cluster.  ID: $KNOWN_ID, IP: $MY_IP"
+                        /bin/etcdctl --endpoint=http://$peer:2379 member remove $KNOWN_ID
+                        if [ $? -eq 1 ]; then
+                            echo "Error removing member in cluster.  ID: $KNOWN_ID, IP: $MY_IP  Exiting..."
+                            exit 1
                         fi
                     fi
-
-                    if [ "$ADD_PEER" -eq 1 ]; then
-                        # add the peer to the cluster
-                        echo "Adding node to the cluster.  IP: $MY_IP"
-                        /bin/etcdctl --endpoint=http://$peer:2379 member add "$APP_NAME-$ID" http://$MY_IP:2380
-#                        if [ $? -eq 1 ]; then
-#                            echo "Error adding member to cluster.  IP: $MY_IP  Exiting..."
-#                            exit 1
-#                        fi
-                    fi
+                    # add the peer to the cluster
+                    echo "Adding node to the cluster.  IP: $MY_IP"
+                    /bin/etcdctl --endpoint=http://$peer:2379 member add "$APP_NAME-$ID" http://$MY_IP:2380
+                    #if [ $? -eq 1 ]; then
+                    #    echo "Error adding member to cluster.  IP: $MY_IP  Exiting..."
+                    #    exit 1
+                    #fi
                 fi
                 break
             done
         fi
 
-        ETCD_PID=""
         echo "Launching etcd into an existing cluster.  IP: $MY_IP.  Cluster: $CLUSTER"
         launch_etcd "$MY_IP" "$ID" "$APP_DATA_DIR" "existing" "$CLUSTER"
     fi
@@ -181,23 +152,65 @@ start_app() {
         exit 1
     fi
 
-    # get container id
-    CONTAINER_ID=`/bin/etcdctl member list | grep "$MY_IP" | awk '{print $1}' | sed 's/\://'`
-    echo "$CONTAINER_ID" > "$CONTAINER_ID_FILE_PATH"
-
     # loop while PID exists
-    while true; do
-        ps -A -o pid | grep "$ETCD_PID" > /dev/null
-        if [ $? -ne 0 ]; then
-            # process stopped, exit
-            break
-        else
-            sleep 5
-        fi
+    while curl -s -m 5 http://localhost:2379/v2/keys > /dev/null
+    do
+      sleep 10
     done
-
     echo "Exiting."
     exit 1
 }
 
-start_app "$FS_PATH" $ID
+
+lock_data_dir() {
+    if [ -z $1 ]; then
+        echo "Directory must be specified."
+        return 1
+    elif [ -z $2 ]; then
+        echo "Node ID must be specified."
+        return 1
+    fi
+
+    DATADIR=$1
+    ID=$2
+    WORKINGDIR="$DATADIR/$ID"
+    cd $1
+    if [ $? -ne 0 ]; then
+        echo "Unable to change into directory $WORKINGDIR"
+        return 1
+    fi
+
+    echo "Attempting to lock: $WORKINGDIR/$LOCKFILE"
+    exec 200>> "$WORKINGDIR/$LOCKFILE"
+    flock -n 200
+    if [ $? -ne 0 ]; then
+        echo "Unable to lock."
+        exec 200>&-
+        return 1;
+    else
+        date 1>&200
+        echo "Lock acquired.  Starting application."
+        start_app "$FS_PATH" "$ID"
+    fi
+}
+
+DATADIR="$FS_PATH/$ID"
+echo "Attempting $DATADIR"
+if [ ! -d "$DATADIR" ]; then
+    r=`mkdir "$DATADIR"`
+    if [ $? -eq 0 ]; then
+        lock_data_dir "$FS_PATH" "$ID"
+        if [ $? -ne 0 ]; then
+            echo "Error locking directory."
+        fi
+    else
+        if [ ! -d "$DATADIR" ]; then
+            echo "Unable to create directory.  System error."
+            exit 1
+        else
+            echo "Another process already created directory."
+        fi
+    fi
+else
+    echo "Directory already taken."
+fi
